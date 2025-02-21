46131ce9ce0a56eb44d19c3b8daaa039cb2caddf
Tengku Arif <arereeftengku@gmail.com>
2025-02-21 21:26:55 +0800

    feat: make dto fields required

--- src/notification/dto/get-user-notifications.dto.ts
+++ src/notification/dto/get-user-notifications.dto.ts
@@ -1,11 +1,13 @@
-import { IsEnum, IsString, IsUUID } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsString, IsUUID } from 'class-validator';
 import { NotificationChannel } from 'src/shared/notification-channels';
 
 export class GetUserNotificationDto {
   @IsString()
   @IsUUID()
+  @IsNotEmpty()
   userId: string;
 
   @IsEnum(NotificationChannel)
+  @IsNotEmpty()
   channel: NotificationChannel;
 }
--- src/notification/dto/send-notification.dto.ts
+++ src/notification/dto/send-notification.dto.ts
@@ -1,15 +1,18 @@
-import { IsEnum, IsString, IsUUID } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsString, IsUUID } from 'class-validator';
 import { NotificationType } from 'src/shared/notification-types';
 
 export class SendNotificationDto {
   @IsString()
   @IsUUID()
+  @IsNotEmpty()
   companyId: string;
 
   @IsString()
   @IsUUID()
+  @IsNotEmpty()
   userId: string;
 
   @IsEnum(NotificationType)
+  @IsNotEmpty()
   type: NotificationType;
 }
